d2a00c09d4e9dc609e99efe1eda0df27adbe4958
TusiaSpy <walendowska.marta@gmail.com>
2017-02-11 22:00:00 +0100

    cokolwiek

--- Project1/src/MarsApplication.java
+++ Project1/src/MarsApplication.java
@@ -6,6 +6,12 @@ public class MarsApplication {
 		spirit.status = "eksploracja";
 		spirit.speed = 2;
 		spirit.temperature = -60;
+		
+		MarsRobot opportunity = new MarsRobot();
+		opportunity.status = "mozliosci";
+		opportunity.speed = 1;
+		opportunity.temperature = -70;
+				
 
 		spirit.showAttributes();
 		System.out.println("Zwiêkszanie prêdkosci do 3.");
@@ -17,6 +23,17 @@ public class MarsApplication {
 		System.out.println("Sprawdzanie temperatury.");
 		spirit.checkTemperature();
 		spirit.showAttributes();
+		
+		opportunity.showAttributes();
+		System.out.println("Zwiêkszanie prêdkosci do 3.");
+		opportunity.speed = 3;
+		opportunity.showAttributes();
+		System.out.println("Zmiana temperatury na -90");
+		opportunity.temperature = -90;
+		opportunity.showAttributes();
+		System.out.println("Sprawdzanie temperatury.");
+		opportunity.checkTemperature();
+		opportunity.showAttributes();
 	}
 
 }
